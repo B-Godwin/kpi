ad710b75c3493e840465a43b9ef2a640e56c68fe
B-Godwin <47323813+B-Godwin@users.noreply.github.com>
2019-03-22 16:53:29 +0000

    Update client.rb
    Env vars use underscores

--- app/aggregators/pingdom/client.rb
+++ app/aggregators/pingdom/client.rb
@@ -3,7 +3,7 @@ module Aggregators
 		class Client
 
 			def self.endpoint
-				ENV['aggregator.pingdom.url']
+				ENV['aggregator_pingdom_url']
 			end
 
 			def self.tagstring(taglist)
@@ -12,14 +12,14 @@ module Aggregators
 
 			# fetch the list of checks bound to the pingdom account, filtered by optional tag
 			def self.fetch_check_list(options = {})
-				get endpoint: "#{endpoint}/2.0/checks", 
+				get endpoint: "#{endpoint}/2.0/checks",
 					params: tagstring(options[:tags])
 			end
 
 
 			# fetch the remote details of a particular check
 			def self.fetch_check_details(options = {})
-				get endpoint: "#{endpoint}/2.1/summary.performance/#{options[:id]}", 
+				get endpoint: "#{endpoint}/2.1/summary.performance/#{options[:id]}",
 					params: tagstring(options[:tags]).merge({
 						resolution: :day,
 						from: options[:from],
@@ -30,11 +30,11 @@ module Aggregators
 
 
 			def self.get(options = {})
-				res = RestClient::Request.execute method: :get, 
+				res = RestClient::Request.execute method: :get,
 											url: options[:endpoint],
-											user: ENV['aggregator.pingdom.user'], 
-											password: ENV['aggregator.pingdom.passwd'],
-											headers: { "app-key" => ENV["aggregator.pingdom.appkey"], "Accept" => "application/json", params: options[:params] }
+											user: ENV['aggregator_pingdom_user'],
+											password: ENV['aggregator_pingdom_passwd'],
+											headers: { "app-key" => ENV["aggregator_pingdom_appkey"], "Accept" => "application/json", params: options[:params] }
 
 				case res.code
 					when 200
